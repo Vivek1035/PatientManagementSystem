6123fc35326a6f604bc5cd499c8f55c19f25fe80
Vivek Singh <coder.vivek44@gmail.com>
2026-05-19 19:26:17 +0530

    Added Dockerfile and Updated folder struture

--- src/main/java/com/pms/patientmanagementsystem/exception/PatientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.pms.patientmanagementsystem.exception;
-
-public class PatientNotFoundException extends RuntimeException {
-    public PatientNotFoundException(String message) {
-        super(message);
-    }
-}
