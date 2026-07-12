57e58ad9c7c2fca310849b2dccc51f244a9ad193
Vivek Singh <coder.vivek44@gmail.com>
2026-07-12 19:45:06 +0530

    Add Redis and monitoring configuration to CDK

--- infrastructure/src/main/java/org.vivek.stack/LocalStack.java
+++ infrastructure/src/main/java/org.vivek.stack/LocalStack.java
@@ -6,11 +6,14 @@ import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ecs.Protocol;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.elasticache.CfnCacheCluster;
+import software.amazon.awscdk.services.elasticache.CfnSubnetGroup;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
+import software.amazon.awscdk.services.servicediscovery.DnsRecordType;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,62 +23,74 @@ import java.util.stream.Collectors;
 public class LocalStack extends Stack {
     private final Vpc vpc;
     private final Cluster ecsCluster;
+    private final CfnCacheCluster elasticCacheCluster;
 
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-    this.vpc = createVpc();
+        this.vpc = createVpc();
 
-    DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
-    DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service-db");
+        DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+        DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service-db");
 
-    CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
-    CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDBHealthCheck");
+        CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
+        CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDBHealthCheck");
 
-    CfnCluster mskCluster = createMskCluster();
+        CfnCluster mskCluster = createMskCluster();
+        this.ecsCluster = createEcsCluster();
+        this.elasticCacheCluster = createRedisCluster();
 
-    this.ecsCluster = createEcsCluster();
+        FargateService authService = createFargateService("AuthService",
+                "auth-service",
+                List.of(4005),
+                authServiceDb,
+                Map.of("JWT_SECRET", "nA3XbIQVPO5HgRFF8eVOOpaJndxueoQvAzMPsRv2WZp"));
 
-    FargateService authService = createFargateService("AuthService",
-            "auth-service",
-            List.of(4005),
-            authServiceDb,
-            Map.of("JWT_SECRET", "nA3XbIQVPO5HgRFF8eVOOpaJndxueoQvAzMPsRv2WZp"));
+        authService.getNode().addDependency(authDbHealthCheck);
+        authService.getNode().addDependency(authServiceDb);
 
-    authService.getNode().addDependency(authDbHealthCheck);
-    authService.getNode().addDependency(authServiceDb);
+        FargateService billingService =
+                createFargateService("BillingService",
+                        "billing-service",
+                        List.of(4001,9001),
+                        null,
+                        null);
 
-    FargateService billingService =
-            createFargateService("BillingService",
-                    "billing-service",
-                    List.of(4001,9001),
-                    null,
-                    null);
+        FargateService analyticsService =
+                createFargateService("AnalyticsService",
+                        "analytics-service",
+                        List.of(4002),
+                        null,
+                        null);
 
-    FargateService analyticsService =
-            createFargateService("AnalyticsService",
-                    "analytics-service",
-                    List.of(4002),
-                    null,
-                    null);
+        analyticsService.getNode().addDependency(mskCluster);
 
-    analyticsService.getNode().addDependency(mskCluster);
+        FargateService patientService = createFargateService("PatientService",
+                "patient-service",
+                List.of(4000),
+                patientServiceDb,
+                Map.of(
+                        "BILLING_SERVICE_ADDRESS", "billing-service.patient-management.local",
+                        "BILLING_SERVICE_GRPC_PORT", "9001"
+                ));
+        patientService.getNode().addDependency(patientDbHealthCheck);
+        patientService.getNode().addDependency(patientServiceDb);
+        patientService.getNode().addDependency(billingService);
+        patientService.getNode().addDependency(mskCluster);
+        patientService.getNode().addDependency(elasticCacheCluster);
 
-    FargateService patientService = createFargateService("PatientService",
-            "patient-service",
-            List.of(4000),
-            patientServiceDb,
-            Map.of(
-                    "BILLING_SERVICE_ADDRESS", "host.docker.internal",
-                    "BILLING_SERVICE_GRPC_PORT", "9001"
-            ));
+        ApplicationLoadBalancedFargateService apiGateway = createApiGatewayService();
+        apiGateway.getNode().addDependency(elasticCacheCluster);
 
-    patientService.getNode().addDependency(patientDbHealthCheck);
-    patientService.getNode().addDependency(patientServiceDb);
-    patientService.getNode().addDependency(billingService);
-    patientService.getNode().addDependency(mskCluster);
+        FargateService prometheusService = createFargateService(
+                "PrometheusService",
+                "prometheus-prod",
+                List.of(9090),
+                null,
+                null
+        );
 
-    createApiGatewayService();
+        createGrafanaService();
     }
 
     private Vpc createVpc() {
@@ -172,6 +187,10 @@ public class LocalStack extends Stack {
         Map<String, String> envVars = new HashMap<>();
         envVars.put("SPRING_KAFKA_BOOTSTRAP_SERVERS", "localhost.localstack.cloud:4510, localhost.localstack.cloud:4511, localhost.localstack.cloud:4512");
 
+        envVars.put("SPRING_CACHE_TYPE", "redis");
+        envVars.put("SPRING_DATA_REDIS_HOST", elasticCacheCluster.getAttrRedisEndpointAddress());
+        envVars.put("SPRING_DATA_REDIS_PORT", elasticCacheCluster.getAttrRedisEndpointPort());
+
         if (additionalEnvVars != null) {
             envVars.putAll(additionalEnvVars);
         }
@@ -199,11 +218,15 @@ public class LocalStack extends Stack {
                 .cluster(ecsCluster)
                 .taskDefinition(taskDefinition)
                 .assignPublicIp(false)
+                .cloudMapOptions(CloudMapOptions.builder()
+                        .name(imageName)
+                        .dnsRecordType(DnsRecordType.A)
+                        .build())
                 .serviceName(imageName)
                 .build();
     }
 
-    private void createApiGatewayService() {
+    private ApplicationLoadBalancedFargateService createApiGatewayService() {
         FargateTaskDefinition taskDefinition =
                 FargateTaskDefinition.Builder.create(this, "APIGatewayTaskDefinition")
                         .cpu(256)
@@ -214,7 +237,9 @@ public class LocalStack extends Stack {
                         .image(ContainerImage.fromRegistry("api-gateway"))
                         .environment(Map.of(
                                 "SPRING_PROFILES_ACTIVE", "prod",
-                                "AUTH_SERVICE_URL", "http://host.docker.internal:4005"
+                                "AUTH_SERVICE_URL", "http://auth-service.patient-management.local:4005",
+                                "REDIS_HOST", elasticCacheCluster.getAttrRedisEndpointAddress(),
+                                "REDIS_PORT", elasticCacheCluster.getAttrRedisEndpointPort()
                         ))
                         .portMappings(List.of(4004).stream()
                                 .map(port -> PortMapping.builder()
@@ -243,8 +268,58 @@ public class LocalStack extends Stack {
                         .taskDefinition(taskDefinition)
                         .desiredCount(1)
                         .healthCheckGracePeriod(Duration.seconds(60))
+                        .publicLoadBalancer(true)
+                        .cloudMapOptions(CloudMapOptions.builder()
+                                .name("api-gateway")
+                                .dnsRecordType(DnsRecordType.A)
+                                .build())
                         .build();
 
+        return apiGateway;
+    }
+
+    public CfnCacheCluster createRedisCluster () {
+        CfnSubnetGroup redisSubnetGroup = CfnSubnetGroup.Builder
+                .create(this, "RedisSubnetGroup")
+                .description("Redis/elasticache subnet group")
+                .subnetIds(vpc.getPrivateSubnets().stream()
+                        .map(ISubnet::getSubnetId)
+                        .collect(Collectors.toList()))
+                .build();
+
+        return CfnCacheCluster.Builder.create(this, "RedisCluster")
+                .cacheNodeType("cache.t2.micro")
+                .engine("redis")
+                .numCacheNodes(1)
+                .cacheSubnetGroupName(redisSubnetGroup.getCacheSubnetGroupName())
+                .vpcSecurityGroupIds(List.of(vpc.getVpcDefaultSecurityGroup()))
+                .build();
+
+    }
+
+    public ApplicationLoadBalancedFargateService createGrafanaService() {
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder
+                .create(this, "GrafanaService")
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .build();
+
+        taskDefinition.addContainer("GrafanaContainer", ContainerDefinitionOptions.builder()
+                .image(ContainerImage.fromRegistry("grafana"))
+                .portMappings(List.of(PortMapping.builder()
+                                .containerPort(3000)
+                        .build()))
+                .build());
+
+        ApplicationLoadBalancedFargateService service = ApplicationLoadBalancedFargateService.Builder
+                .create(this, "GrafanaUIService")
+                .taskDefinition(taskDefinition)
+                .desiredCount(1)
+                .listenerPort(3000)
+                .publicLoadBalancer(true)
+                .build();
+
+        return service;
     }
 
     public static void main(String[] args) {
